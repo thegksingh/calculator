4213877ddd5a81b3c16bc7aa6b6903cb15fbb338
gaurav <gksingh1106@gmail.com>
2026-07-01 15:06:20 +0530

    feat: add divide function and handle zero division

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ function subtract(num1, num2){
 function multiply(num1, num2){
     return num1 * num2;
 }
+
+function divide(num1, num2){
+    if (num2 === 0){
+        return num1 + " can't be divided by 0"
+    }
+    return num1 / num2;
+}
