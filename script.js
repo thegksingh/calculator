c3c51d2300bdd8f9a2adcd124715ed6ed97e129d
gaurav <gksingh1106@gmail.com>
2026-07-01 15:24:41 +0530

    feat: add operate function
    Create a switch statement that takes two numbers and an operator, evaluates the operator, and calls the corresponding math function (add, subtract, multiply, divide) to return the final calculation.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function add(num1, num2){
     return num1 + num2;
 }
 
-
 function subtract(num1, num2){
     return num1 - num2;
 }
@@ -17,3 +16,20 @@ function divide(num1, num2){
     }
     return num1 / num2;
 }
+
+function operate(num1, operater, num2){
+    switch(operater){
+        case "+":
+            return add(num1, num2);
+        
+        case "-":
+            return subtract(num1, num2);
+
+        case "*":
+            return multiply(num1, num2);
+        
+        case "/":
+            return divide(num1,num2);
+    }
+}
+
